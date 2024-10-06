37c608845e73ea8917a275591e9b08424ed9b3c9
ackwolver335 <abhaych335@gmail.com>
2024-10-06 13:18:31 +0530

    Introduction to Python Strings

---
+++ Day1/pyStrings.py
@@ -0,0 +1,17 @@
+# Getting Started with Strings
+text = "this is some textual data here"
+
+print(text)                                 # here we have the output for it
+
+# getting particular partition from the string using slicing and indexes
+print(text[3])
+
+# the above one will output just the first 3 characters from the string
+# getting another partition for the data
+print(text[14:])                                # all the string part after 14 position
+
+# we can also define the strings here using '' single quotes
+print('This is another string with single quotes')
+
+# Here let us create a number and add it to a string
+print("The number here is : " + str(45))
