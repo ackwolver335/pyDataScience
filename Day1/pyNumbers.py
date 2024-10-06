f8ea62439de1277f270649707270aeaa26939e19
ackwolver335 <abhaych335@gmail.com>
2024-10-06 13:09:54 +0530

    Introduction to Numbers in Python

---
+++ Day1/pyNumbers.py
@@ -0,0 +1,33 @@
+# let us understand about the Numbers or Numerical Data Type
+
+# getting started with defining a performing different operations on it
+num1 = 23
+num2 = 44
+
+# getting the sum of these two numbers
+print(num1 + num2)
+
+# similarly we have further diffferent operations on it
+
+print(num1 - num2)          # subtraction
+print(num1 ** 2)            # power of the number
+print(num1 / num2)          # division operation
+
+# creating a variable for storing the distance of the city
+distance_city = 188
+distance_city2 = 247
+
+total_distance = distance_city + distance_city2
+print(total_distance)                                   # distance calculations
+
+# finding the time overall with the given speed
+mph = 65
+time = total_distance / mph
+
+# getting the time with 2 decimal points
+print("The time regarding it is : " + str(round(time,2)))
+
+# some different operations accordingly operator precedence
+print(34 - (45 + 6) * 56)
+
+# these were some mathematical operations that we can perform over to the Numbers in Python
