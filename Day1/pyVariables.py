b2e077276db275da3adae7923b7622e77048b6c7
ackwolver335 <abhaych335@gmail.com>
2024-10-06 13:01:24 +0530

    Intro to Python Variables

---
+++ Day1/pyVariables.py
@@ -0,0 +1,26 @@
+# variable in Python : these are some containers that are used in order to store data or values
+
+# here let us create a variables together by assigning values to them.
+rent = 23
+
+gas = 293.2                         # float data type variables
+groceries = 305.6                   # groceries values
+
+print(rent)
+
+total = rent + gas + groceries      # getting the total of it
+print(total)
+
+# also we can create different variables with textual values
+item1 = "this is some item with textual value"
+item2 = "this is another one"
+
+# on taking variable's values we can use the already assigned names to the Python reserved keywords
+"""
+    defining a variable with name print will provide you an error
+
+    print = 34          # this will surely provide you an error when the code is interpreted
+    Note : But we can change the value once it is assigned to it
+"""
+
+# in further programs we will understand the use of Further different data types here
